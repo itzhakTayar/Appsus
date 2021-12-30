b8aa096381f2c16b3127338a15f1e573081a3380
Noam Shir <noamshirm@gmail.com>
2021-12-30 21:16:33 +0200

    noams notes

--- js/apps/keep/cmps/note-preview.jsx
+++ js/apps/keep/cmps/note-preview.jsx
@@ -1,19 +1,26 @@
-import { DynamicCmp } from './DynamicCmp.jsx';
+import { DynamicCmp } from "./DynamicCmp.jsx";
 
 export class NotesPreview extends React.Component {
   state = {
     isShowNoteModal: false,
     note: this.props.note,
+    hover: false,
   };
 
   render() {
+    var { id } = this.state.note;
+    console.log(this.state.hover);
+    var isHover = this.state.hover;
     return (
-      <div>
-        <article>
-          <div className="note-container">
-            <DynamicCmp note={this.state.note} />
-          </div>
-        </article>
+      <div
+        className="note-item"
+        onMouseEnter={() => {
+          this.setState({ hover: true });
+        }}
+        onMouseLeave={() => this.setState({ hover: false })}
+      >
+        <DynamicCmp note={this.state.note} />
+        {isHover && <h1>Hello Hover</h1>}
       </div>
     );
   }
