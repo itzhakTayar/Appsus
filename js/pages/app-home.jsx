6011414da0dc3c21d7cc6796e2c5897c226469ed
ItzhakTayar <itzhaktayar1@gmail.com>
2022-01-01 15:31:06 +0200

    p

--- js/pages/app-home.jsx
+++ js/pages/app-home.jsx
@@ -3,6 +3,20 @@ export class Home extends React.Component {
     return (
       <section className="home">
         <h1>Welcome Visitor</h1>
+        {/* fa email */}
+        <button className="fa-test fas fa-trash"></button>
+        <button className="fa-test fas fa-expand"></button>
+        <button className="fa-test fas fa-star"></button>
+        <button className="fa-test fas fa-paper-plane"></button>
+        <button className="fa-test fas fa-search"></button>
+        <button className="fa-test fas fa-pencil-alt"></button>
+        <button className="fa-test fas fa-envelope-open"></button>
+        {/* fa notes */}
+        <button className="fa-test fas fa-thumbtack"></button>
+        <button className="fa-test fas fa-palette"></button>
+        <button className="fa-test fas fa-envelope"></button>
+        <button className="fa-test fas fa-edit"></button>
+        <button className="fa-test fas fa-trash-alt"></button>
       </section>
     );
   }
